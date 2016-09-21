5f2599959fca1dd9a509fceb8d68e8820539dfb0
nosoop <nosoop@users.noreply.github.com>
2016-09-20 21:50:29 -0700

    Python 3 and yes, these languages exist
    Migrated to Python 3 syntax, as well as made the script follow some
    proper conventions for file names (directories end with the path
    separator and relative paths do not start with it).
    
    Also added Bulgarian, Thai, and Russian.

--- utilities/localization_db.py
+++ utilities/localization_db.py
@@ -5,34 +5,37 @@ import os
 # Prereq:
 # pip install vdf
 
-GAME_DIR = r'/path/to/tf'
-LANGUAGE_DB = GAME_DIR + '/addons/sourcemod/data/sqlite/language-db.sq3'
+GAME_DIR = r'/path/to/tf/'
+LANGUAGE_DB = GAME_DIR + 'addons/sourcemod/data/sqlite/language-db.sq3'
 
 LANGUAGE_FILES = [
-	GAME_DIR + '/resource/tf_brazilian.txt',
-	GAME_DIR + '/resource/tf_czech.txt',
-	GAME_DIR + '/resource/tf_danish.txt',
-	GAME_DIR + '/resource/tf_dutch.txt',
-	GAME_DIR + '/resource/tf_english.txt',
-	GAME_DIR + '/resource/tf_finnish.txt',
-	GAME_DIR + '/resource/tf_french.txt',
-	GAME_DIR + '/resource/tf_german.txt',
-	GAME_DIR + '/resource/tf_greek.txt',
-	GAME_DIR + '/resource/tf_hungarian.txt',
-	GAME_DIR + '/resource/tf_italian.txt',
-	GAME_DIR + '/resource/tf_japanese.txt',
-	GAME_DIR + '/resource/tf_korean.txt',
-	# GAME_DIR + '/resource/tf_koreana.txt', # (diffing this with tf_korean doesn't show any differences)
-	GAME_DIR + '/resource/tf_norwegian.txt',
-	GAME_DIR + '/resource/tf_polish.txt',
-	GAME_DIR + '/resource/tf_portuguese.txt',
-	GAME_DIR + '/resource/tf_romanian.txt',
-	GAME_DIR + '/resource/tf_schinese.txt',
-	GAME_DIR + '/resource/tf_spanish.txt',
-	GAME_DIR + '/resource/tf_swedish.txt',
-	GAME_DIR + '/resource/tf_tchinese.txt',
-	GAME_DIR + '/resource/tf_turkish.txt',
-	GAME_DIR + '/resource/tf_ukrainian.txt',
+	GAME_DIR + 'resource/tf_brazilian.txt',
+	GAME_DIR + 'resource/tf_bulgarian.txt',
+	GAME_DIR + 'resource/tf_czech.txt',
+	GAME_DIR + 'resource/tf_danish.txt',
+	GAME_DIR + 'resource/tf_dutch.txt',
+	GAME_DIR + 'resource/tf_english.txt',
+	GAME_DIR + 'resource/tf_finnish.txt',
+	GAME_DIR + 'resource/tf_french.txt',
+	GAME_DIR + 'resource/tf_german.txt',
+	GAME_DIR + 'resource/tf_greek.txt',
+	GAME_DIR + 'resource/tf_hungarian.txt',
+	GAME_DIR + 'resource/tf_italian.txt',
+	GAME_DIR + 'resource/tf_japanese.txt',
+	GAME_DIR + 'resource/tf_korean.txt',
+	# GAME_DIR + 'resource/tf_koreana.txt', # (diffing this with tf_korean doesn't show any differences)
+	GAME_DIR + 'resource/tf_norwegian.txt',
+	GAME_DIR + 'resource/tf_polish.txt',
+	GAME_DIR + 'resource/tf_portuguese.txt',
+	GAME_DIR + 'resource/tf_romanian.txt',
+	GAME_DIR + 'resource/tf_russian.txt',
+	GAME_DIR + 'resource/tf_schinese.txt',
+	GAME_DIR + 'resource/tf_spanish.txt',
+	GAME_DIR + 'resource/tf_swedish.txt',
+	GAME_DIR + 'resource/tf_tchinese.txt',
+	GAME_DIR + 'resource/tf_thai.txt',
+	GAME_DIR + 'resource/tf_turkish.txt',
+	GAME_DIR + 'resource/tf_ukrainian.txt'
 ]
 
 # Some of the tokens used in other language refer to the English-language version.
@@ -57,9 +60,8 @@ total_local_strings = 0
 for localization_file in LANGUAGE_FILES:
 	# Decode VDF.  It has UCS2 encoding, so decode it as such
 	tokens_included = 0
-	with open(localization_file, 'r') as f:
-	    data = vdf.parse(f.read().decode('UTF-16LE'))
-	    data = data['lang']
+	data = vdf.parse(open(localization_file, 'r', encoding='UTF-16LE'))
+	data = data['lang']
 	
 	language = data['Language'].lower()
 	for k, v in data['Tokens'].items():
@@ -68,12 +70,12 @@ for localization_file in LANGUAGE_FILES:
 			tokens_included += 1
 
 	db.commit()
-	print 'Localization file for {} ({}) has {} string entries (inserted {})'.format(data['Language'], os.path.basename(localization_file), len(data['Tokens']), tokens_included)
+	print('Localization file for {} ({}) has {} string entries (inserted {})'.format(data['Language'], os.path.basename(localization_file), len(data['Tokens']), tokens_included))
 	total_local_strings += tokens_included
 
 # Just do some housekeeping for size
-print 'Performing a VACUUM on the database.'
+print('Performing a VACUUM on the database.')
 dbc.execute('VACUUM')
 db.commit()
 
-print '{} localization strings submitted to database.'.format(total_local_strings)
+print('{} localization strings submitted to database.'.format(total_local_strings))
